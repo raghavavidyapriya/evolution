28f4be57173860c13d814667324b6291db16edfa
Raghava <raghavavidyapriya@gmail.com>
2025-01-28 23:21:40 +0530

    updated backend url in frontend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://evolution-backend-w1pe.onrender.com"
     const [token, setToken] = useState("")
     const [cars_list, setCarsList] = useState([])
 
